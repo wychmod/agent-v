4a635265786ef7032b85cfaddf5999c8e92ada4f
wychmod <545480453@qq.com>
2026-02-21 10:21:54 +0800

    feat(api): 添加mcp客户端异步调用示例脚本
    - 引入AsyncExitStack实现异步上下文管理
    - 使用streamablehttp_client建立底层传输连接
    - 初始化ClientSession进行会话管理
    - 列出可用工具并打印工具名称列表
    - 调用chatCompletions工具并输出结果内容
    - 使用asyncio运行主异步函数main

---
+++ agent-study/24.外部api搜索.py
@@ -0,0 +1,38 @@
+from contextlib import AsyncExitStack
+
+from mcp import ClientSession
+from mcp.client.streamable_http import streamablehttp_client
+
+
+async def main():
+    base_url = "https://qianfan.baidubce.com/v2/ai_search/mcp"
+    header = {"Authorization": "Bearer bce-v3/ALTAK-x22AOpJuk3rLFAtxykPVs/5d7c7522f86c47c9e2b1725f590b5f9d2b33f637"}
+
+    exit_stack = AsyncExitStack()
+
+    try:
+        transport = await exit_stack.enter_async_context(streamablehttp_client(
+            url=base_url,
+            headers=header
+        ))
+
+        read_stream, write_stream, _ = transport
+
+        session: ClientSession = await exit_stack.enter_async_context(ClientSession(read_stream, write_stream))
+
+        await session.initialize()
+
+        list_tools = await session.list_tools()
+        print("工具列表:", [tool.name for tool in list_tools.tools])
+        result = await session.call_tool("chatCompletions", {
+            "query": "元神官网"
+        })
+        print("结果:", result.content[0].text)
+    finally:
+        await exit_stack.aclose()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
